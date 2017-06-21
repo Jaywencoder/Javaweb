158ed7272fb25978fda3577806b0af67146be1b1
jay <jaywencoder@gmai.com>
2017-06-22 00:44:50 +1000

    Spring basic code

--- spring/src/com/how2java/test/TestSpring.java
+++ spring/src/com/how2java/test/TestSpring.java
@@ -5,14 +5,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+import com.how2java.service.ProductService;
 
 public class TestSpring {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
 //		ApplicationContext context = new FileSystemXmlApplicationContext("fileapplicationContext.xml");
-		Category c = (Category) context.getBean("c");
+//		Category c = (Category) context.getBean("c");
 //
-		System.out.println(c.getName());
+//		System.out.println(c.getName());
+//		Product p = (Product) context.getBean("p");
+//        System.out.println(p.getName());
+//        System.out.println(p.getCategory().getName());
+        ProductService s = (ProductService) context.getBean("s");
+        s.doSomeService();
 	}
 }
